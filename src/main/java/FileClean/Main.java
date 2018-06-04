da4f8c8057cbf83b7f25b0d245ff49992cf12ca0
bigbeard <bigbeard@126.com>
2018-06-04 19:19:29 +0800

    修改了类声明

--- src/main/java/FileClean/Main.java
+++ src/main/java/FileClean/Main.java
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("文件清理工具");
         frame.setSize(800, 500);
-        frame.setContentPane(new MainForm().getPnlRoot());
+        MainForm mainForm = new MainForm();
+        frame.setContentPane(mainForm.$$$getRootComponent$$$());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
