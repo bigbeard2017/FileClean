c4837f7f513b4efed8fe3cef71023b8a0723ce29
bigbeard <bigbeard@126.com>
2018-06-06 09:08:03 +0800

    1 增加了依赖包 2 修改了默认图片

--- src/main/java/FileClean/Main.java
+++ src/main/java/FileClean/Main.java
@@ -9,6 +9,13 @@ public class Main {
         JFrame frame = new JFrame("文件清理工具");
         frame.setSize(800, 500);
         MainForm mainForm = new MainForm();
+        ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("timg.jpeg"));  //xxx代表图片存放路径，2.png图片名称及格式
+        frame.setIconImage(icon.getImage());
+//        try {
+//            UIManager.setLookAndFeel(new com.sun.java.swing.plaf.gtk.GTKLookAndFeel());
+//        } catch (UnsupportedLookAndFeelException e) {
+//            e.printStackTrace();
+//        }
         frame.setContentPane(mainForm.$$$getRootComponent$$$());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
